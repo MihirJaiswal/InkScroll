75f90ba9bcfe38422922e7a2f0d4dcdd7b96c23a
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-06-19 01:14:59 +0530

    some changes

--- mangaread-frontend/src/app/components/CommentSection.tsx
+++ mangaread-frontend/src/app/components/CommentSection.tsx
@@ -139,8 +139,8 @@ const CommentSection: React.FC<CommentSectionProps> = ({ mangaTitle, comments, i
               </div>
             </div>
             {showModal && commentIdToDelete === comment._id && (
-              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 rounded-md z-50">
-                <div className="bg-white dark:bg-gray-800 border border-black p-6 rounded-lg shadow-lg max-w-md mx-auto">
+              <div className="absolute inset-0 flex items-center justify-center md:bg-black md:bg-opacity-30 rounded-md z-50">
+                <div className="bg-white dark:bg-gray-800 border border-black p-6 rounded-lg shadow-lg max-w-md mx-auto mr-4 md:mr-0">
                   <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200 mb-4">Confirm Deletion</h3>
                   <p className="text-gray-600 dark:text-gray-300 mb-6">Are you sure you want to delete this comment?</p>
                   <div className="flex justify-end space-x-4">
