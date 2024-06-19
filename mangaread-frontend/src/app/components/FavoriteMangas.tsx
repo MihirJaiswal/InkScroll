63c07a798eda0c6a8e719ca7ceb9511f6aba84d6
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-06-19 13:42:54 +0530

    some changes

--- mangaread-frontend/src/app/components/FavoriteMangas.tsx
+++ mangaread-frontend/src/app/components/FavoriteMangas.tsx
@@ -18,7 +18,7 @@ const FavoriteMangas: React.FC = () => {
   const [filteredFavorites, setFilteredFavorites] = useState<Manga[]>([]); // State for filtered mangas
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [searchTerm, setSearchTerm] = useState(''); // State for search term
-  const [sortType, setSortType] = useState<'title' | 'rating'>('title'); // State for sort type
+  const [sortType, setSortType] = useState<'title' | 'rating' | 'sorthere'>('sorthere'); // State for sort type
 
   useEffect(() => {
     const fetchFavorites = async () => {
@@ -103,6 +103,7 @@ const FavoriteMangas: React.FC = () => {
               onChange={(e) => handleSortChange(e.target.value as 'title' | 'rating')}
               className="px-4 py-2 w-72 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
             >
+              <option>Sort Here</option>
               <option value="title">Sort by Title</option>
               <option value="rating">Sort by Rating</option>
             </select>
